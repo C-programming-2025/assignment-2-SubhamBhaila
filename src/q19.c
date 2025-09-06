b6635dbc6e962c079cdc1421e113cc121d1780fe
SubhamBhaila <subhambhaila10@gmail.com>
2025-09-06 21:56:05 +0545

    Update q19.c

--- src/q19.c
+++ src/q19.c
@@ -1,5 +1,13 @@
-/* Write a C program that prompts the user for a positive integer and prints a pattern of asterisks (*) in a square shape using nested loops.
-Example: User’s Input = 2, then pattern to print will be:
-    * *
-    * *
-*/
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
