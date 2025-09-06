606028c6e06eb923f03d3ed7c5400be4492c1083
SubhamBhaila <subhambhaila10@gmail.com>
2025-09-06 21:51:17 +0545

    Update q11.c

--- src/q11.c
+++ src/q11.c
@@ -1,5 +1,12 @@
-/* Write a C program that prints a pattern of asterisks.
-    *****
-     ***
-      *
-*/
+#include <stdio.h>
+int main() {
+    int i, j, space, rows = 3;
+    for (i = rows; i >= 1; i--) {
+        for (space = rows; space > i; space--)
+            printf(" ");
+        for (j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
